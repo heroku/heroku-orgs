070b1df4c9d536df01d946876415fb4f8cf218ce
dickeyxxx <jeff@dickeyxxx.com>
2016-01-28 17:08:29 -0800

    added orgs:index

---
+++ test/commands/orgs/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let cmd = require('../../../commands/orgs');
+
+describe('heroku orgs', () => {
+  beforeEach(() => cli.mockConsole());
+  afterEach(()  => nock.cleanAll());
+
+  it('shows the orgs', () => {
+    let api = nock('https://api.heroku.com:443')
+    .get('/organizations')
+    .reply(200, [
+      {name: 'org a', role: 'collaborator'},
+      {name: 'org b', role: 'admin'},
+    ]);
+    return cmd.run({flags: {}})
+    .then(() => expect(
+`Organization  Role
+────────────  ────────────
+org a         collaborator
+org b         admin
+`).to.eq(cli.stdout))
+      .then(() => expect(``).to.eq(cli.stderr))
+      .then(() => api.done());
+  });
+});
