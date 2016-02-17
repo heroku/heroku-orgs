6699c801823b5b8aaa518b6bf028999cdfeb622c
dickeyxxx <jeff@dickeyxxx.com>
2016-02-16 16:03:55 -0800

    use v3 endpoint for leave

--- commands/apps/leave.js
+++ commands/apps/leave.js
@@ -5,25 +5,10 @@ let co  = require('co');
 
 function* run (context, heroku) {
   let app = yield heroku.get(`/apps/${context.app}`).catch(() => null);
-  let request;
-  if (app && app.owner.email.endsWith('@herokumanager.com')) {
-    request = heroku.request({
-      method:     'DELETE',
-      path:       `/v1/app/${context.app}/join`,
-      parseJSON:  false,
-      headers:    {Accept: 'application/json'}
-    }).catch(function (err) {
-      throw new Error(err.body);
-    });
-  } else {
-    request = heroku.get('/account')
-    .then(function (user) {
-      return heroku.request({
-        method:     'DELETE',
-        path:       `/apps/${context.app}/collaborators/${encodeURIComponent(user.email)}`,
-      });
-    });
-  }
+  let user = yield heroku.get('/account');
+  let path = `/apps/${context.app}/collaborators/${encodeURIComponent(user.email)}`;
+  if (app && app.owner.email.endsWith('@herokumanager.com')) path = '/organizations' + path;
+  let request = heroku.request({method: 'DELETE', path});
   yield cli.action(`Leaving ${cli.color.cyan(context.app)}`, request);
 }
 
