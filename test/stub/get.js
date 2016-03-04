91cb11c6db36374ada9a5c72fea7634ff66c7044
Raul Barroso <ra.barroso@gmail.com>
2016-03-03 18:04:02 -0800

    Add tests for orgs with static-permissions

--- test/stub/get.js
+++ test/stub/get.js
@@ -1,3 +1,73 @@
+function appCollaborators() {
+  return nock('https://api.heroku.com:443')
+  .get('/apps/myapp/collaborators')
+  .reply(200, [
+    {user: {email: 'raulb@heroku.com'}, role: 'owner'},
+    {user: {email: 'jeff@heroku.com'},  role: 'collaborator'},
+  ]);
+}
+
+function appPrivileges() {
+  return nock('https://api.heroku.com:443', {
+    reqheaders: {Accept: 'application/vnd.heroku+json; version=3.org-privileges'}
+  })
+  .get('/organizations/privileges')
+  .reply(200, [
+    { name: 'deploy' },
+    { name: 'manage' },
+    { name: 'operate' },
+    { name: 'view' }
+  ]);
+}
+
+function orgApp() {
+  return nock('https://api.heroku.com:443')
+  .get('/apps/myapp')
+  .reply(200, {
+    name: 'myapp',
+    owner: { email: 'myorg@herokumanager.com' }
+  });
+}
+
+function orgAppCollaborators() {
+  return nock('https://api.heroku.com:443', {
+    reqheaders: {Accept: 'application/vnd.heroku+json; version=3.org-privileges'}
+  })
+  .get('/organizations/apps/myapp/collaborators')
+  .reply(200, [
+    {
+      role: 'owner',
+      user: { email: 'myorg@herokumanager.com' }
+    },
+    {
+      role: 'collaborator',
+      user: { email: 'bob@heroku.com' }
+    },
+    {
+      role: 'admin',
+      user: { email: 'raulb@heroku.com' }
+    }
+  ]);
+}
+
+function orgAppCollaboratorsWithPrivileges() {
+  return nock('https://api.heroku.com:443', {
+    reqheaders: {Accept: 'application/vnd.heroku+json; version=3.org-privileges'}
+  })
+  .get('/organizations/apps/myapp/collaborators')
+  .reply(200, [
+    { privileges: [],
+      role: 'owner',
+      user: { email: 'myorg@herokumanager.com' }
+    },
+    {
+      privileges: [ { name: 'deploy' }, { name: 'view' } ],
+      role: 'viewer',
+      user: { email: 'bob@heroku.com' }
+    }
+  ]);
+}
+
 function orgFlags(flags) {
   return nock('https://api.heroku.com:443', {
     reqheaders: {Accept: 'application/vnd.heroku+json; version=2'}
@@ -8,13 +78,23 @@ function orgFlags(flags) {
   });
 }
 
-function orgApp() {
+function orgMembers() {
   return nock('https://api.heroku.com:443')
-  .get('/apps/myapp')
-  .reply(200, {
-    name: 'myapp',
-    owner: { email: 'myorg@herokumanager.com' }
-  });
+  .get('/organizations/myorg/members')
+  .reply(200, [
+      {
+        email: 'raulb@heroku.com', role: 'admin',
+        user: { email: 'raulb@heroku.com' }
+      },
+      {
+        email: 'bob@heroku.com', role: 'viewer',
+        user: { email: 'bob@heroku.com' }
+      },
+      {
+        email: 'peter@heroku.com', role: 'collaborator',
+        user: { email: 'peter@heroku.com' }
+      }
+  ]);
 }
 
 function personalApp() {
@@ -27,7 +107,12 @@ function personalApp() {
 }
 
 module.exports = {
-  orgFlags: orgFlags,
+  appCollaborators: appCollaborators,
+  appPrivileges: appPrivileges,
   orgApp: orgApp,
+  orgAppCollaborators: orgAppCollaborators,
+  orgAppCollaboratorsWithPrivileges: orgAppCollaboratorsWithPrivileges,
+  orgFlags: orgFlags,
+  orgMembers: orgMembers,
   personalApp: personalApp
 };
