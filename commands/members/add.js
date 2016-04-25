415140ecc674e54e953e3c5c502c95f20b4d33e7
Eric Black <eblack@heroku.com>
2016-04-25 14:46:08 -0700

    Fetch members and features, and then log a warning message if user goes over free limit.

--- commands/members/add.js
+++ commands/members/add.js
@@ -5,9 +5,13 @@ let co     = require('co');
 let extend = require('util')._extend;
 
 function* run (context, heroku) {
+  const FREE_TEAM_LIMIT = 5;
   let org   = context.org;
   let email = context.args.email;
   let role  = context.flags.role;
+  let members = yield heroku.get(`/organizations/${org}/members`);
+  let features = yield heroku.get('/account/features');
+  let orgCreationFeature = features.find(function(feature) { return feature.name === 'standard-org-creation'});
 
   let request = heroku.request({
     method: 'PUT',
@@ -15,6 +19,9 @@ function* run (context, heroku) {
     body:   {email, role},
   });
   yield cli.action(`Adding ${cli.color.cyan(email)} to ${cli.color.magenta(org)} as ${cli.color.green(role)}`, request);
+  if ((members.length === FREE_TEAM_LIMIT) && orgCreationFeature) {
+    cli.log("You'll be billed monthly for teams over 5 members.");
+  }
 }
 
 let cmd = {
