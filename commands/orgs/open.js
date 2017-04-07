5602f3f8ea76c9ef8d4acfe4a498509dce5f6a53
Doug McInnes <doug@dougmcinnes.com>
2017-04-07 11:52:47 -0700

    deprecate orgs:open command

--- commands/orgs/open.js
+++ commands/orgs/open.js
@@ -4,15 +4,14 @@ let cli = require('heroku-cli-util')
 let co = require('co')
 
 function * run (context, heroku) {
-  let org = yield heroku.get(`/organizations/${context.org}`)
-  yield cli.open(`https://dashboard.heroku.com/orgs/${org.name}`)
+  cli.error(`orgs:open is no longer in the CLI.
+Please use the "teams:open" command instead.
+See ${cli.color.cyan('https://devcenter.heroku.com/articles/heroku-teams')} for more info.`)
 }
 
 module.exports = {
   topic: 'orgs',
   command: 'open',
-  description: 'open the organization interface in a browser window',
-  needsAuth: true,
-  needsOrg: true,
+  hidden: true,
   run: cli.command(co.wrap(run))
 }
