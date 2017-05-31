2f97ff84c0e1783d7d7d0a6f4a2fe316d7fbc8aa
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-05-31 12:46:47 -0700

    updates

--- commands/members/remove.js
+++ commands/members/remove.js
@@ -65,6 +65,7 @@ module.exports = {
   wantsOrg: true,
   args: [{name: 'email'}],
   flags: [
+    // flags.org({name: 'org', hasValue: true, description: 'org to use', hidden: false}),
     flags.team({name: 'team', hasValue: true, hidden: true})
   ],
   run: cli.command(co.wrap(run))
