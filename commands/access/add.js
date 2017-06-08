7e1c586753325d32fc12e67fbfae62b6e4aef828
Matthew Origer <moriger@heroku.com>
2017-06-08 14:12:31 -0500

    adjust help text spacing and import fix

--- commands/access/add.js
+++ commands/access/add.js
@@ -47,11 +47,9 @@ module.exports = [
     needsApp: true,
     command: 'add',
     description: 'Add new users to your app',
-    help: `
-Examples:
+    help: `Examples:
 
     heroku access:add user@email.com --app APP # Add a collaborator to your app
-		
     heroku access:add user@email.com --app APP --permissions deploy,manage,operate # permissions must be comma separated`,
     args: [{name: 'email', optional: false}],
     flags: [
